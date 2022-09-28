a7adc0bbb89ebc1b01b42788186105e35253cda3
Vincent Varoquaux <vincent.varoquaux@airbus.com>
2022-09-28 14:19:33 +0200

    v1.0.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def parse_requirements(file):
 
 setuptools.setup(
     name="geocube_client",
-    version="0.6.0",
+    version="1.0.0",
     author="Varoquaux Vincent",
     author_email="vincent.varoquaux[at]airbus.com",
     description="Geocube Python Client library",
